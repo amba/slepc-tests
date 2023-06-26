711dc3a967f1c97d50cb25edffadcac7952b425b
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2023-06-26 19:54:25 +0200

    output file

--- 1d-josephson.c
+++ 1d-josephson.c
@@ -1,16 +1,11 @@
-/*
-   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   SLEPc - Scalable Library for Eigenvalue Problem Computations
-   Copyright (c) 2002-, Universitat Politecnica de Valencia, Spain
-
-   This file is part of SLEPc.
-   SLEPc is distributed under a 2-clause BSD license (see LICENSE).
-   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-*/
-
 static char help[] = "Standard symmetric eigenproblem corresponding to the Laplacian operator in 1 dimension.\n\n"
   "The command line options are:\n"
   "  -n <n>, where <n> = number of grid subdivisions = matrix dimension.\n\n";
+
+static PetscReal const_hbar = 1.0545718176461565e-34;
+static PetscReal const_e = 1.602176634e-19;
+static PetscReal const_m_e = 9.1093837015e-31;
+
 
 #include <slepceps.h>
 
@@ -19,13 +14,11 @@ int main(int argc,char **argv)
   Mat            H;           /* BdG Hamiltonian */
   EPS            eps;         /* eigenproblem solver context */
   ST             st;          /* spectral transformation context */
-  EPSType        type;
-  PetscReal      error,tol;
   PetscScalar    kr,ki;
-  PetscScalar gap, sc_gap = 0.1, mu = 1;
-  PetscInt       i,nev,maxit,its,nconv;
-  PetscInt       N_sites = 1000;
-  
+  PetscScalar gap, sc_gap = 0.1, mu = 1.234;
+  PetscInt       i,its,nconv;
+  PetscInt       N_evs = 40, N_sites = 1000;
+  FILE *file;
   PetscMPIInt mpi_size;
   
   PetscFunctionBeginUser;
@@ -37,6 +30,8 @@ int main(int argc,char **argv)
 
   PetscPrintf(PETSC_COMM_WORLD,"sizeof(petsc scalar): %lu, sizeof(petsc real): %lu\n", sizeof(PetscScalar), sizeof(PetscReal));
 
+  /* Open file */
+  PetscCall(PetscFOpen(PETSC_COMM_WORLD, "output.dat", "w" , &file));
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Compute the operator matrix that defines the eigensystem, H_{BdG}Φ = EΦ
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -97,7 +92,7 @@ int main(int argc,char **argv)
   */
   PetscCall(EPSGetST(eps,&st));
   PetscCall(STSetType(st,STSINVERT));
-  PetscCall(EPSSetDimensions(eps, 10, PETSC_DECIDE, PETSC_DECIDE));
+  PetscCall(EPSSetDimensions(eps, N_evs, PETSC_DECIDE, PETSC_DECIDE));
   PetscCall(EPSSetTarget(eps, 0));
   PetscCall(EPSSetTolerances(eps, 1e-2, 1000));  
   PetscCall(EPSSetFromOptions(eps));
@@ -105,7 +100,7 @@ int main(int argc,char **argv)
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Solve the eigensystem
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  for (PetscReal Phi = -3.141; Phi < 3.141; Phi += 0.1) {
+  for (PetscReal Phi = -2*3.141; Phi < 2*3.141; Phi += 0.1) {
     for (i=N_sites/2; i < N_sites; ++i) {
       gap = sc_gap * PetscExpComplex(PETSC_i * Phi);
       PetscCall(MatSetValue(H,2*i,2*i+1, gap, INSERT_VALUES));
@@ -115,7 +110,6 @@ int main(int argc,char **argv)
     PetscCall(MatAssemblyEnd(H,MAT_FINAL_ASSEMBLY));
    
     PetscCall(EPSSetOperators(eps,H,NULL));
-    /* PetscCall(EPSSetUp(eps)); */
     PetscCall(EPSSolve(eps));
     
     /*
@@ -123,53 +117,18 @@ int main(int argc,char **argv)
     */
     PetscCall(EPSGetIterationNumber(eps,&its));
     PetscCall(PetscPrintf(PETSC_COMM_WORLD," Number of iterations of the method: %" PetscInt_FMT "\n",its));
-    PetscCall(EPSGetType(eps,&type));
-    PetscCall(PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type));
-    PetscCall(EPSGetDimensions(eps,&nev,NULL,NULL));
-    PetscCall(PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %" PetscInt_FMT "\n",nev));
-    PetscCall(EPSGetTolerances(eps,&tol,&maxit));
-    PetscCall(PetscPrintf(PETSC_COMM_WORLD," Stopping condition: tol=%.4g, maxit=%" PetscInt_FMT "\n",(double)tol,maxit));
 
-    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-       Display solution and clean up
-       - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-    /*
-      Get number of converged approximate eigenpairs
-    */
     PetscCall(EPSGetConverged(eps,&nconv));
     PetscCall(PetscPrintf(PETSC_COMM_WORLD," Number of converged eigenpairs: %" PetscInt_FMT "\n\n",nconv));
 
-    if (nconv>0) {
-      /*
-        Display eigenvalues and relative errors
-      */
-      PetscCall(PetscPrintf(PETSC_COMM_WORLD,
-                            "           k          ||Ax-kx||/||kx||\n"
-                            "   ----------------- ------------------\n"));
-
-      for (i=0;i<nconv;i++) {
-        /*
-          Get converged eigenpairs: i-th eigenvalue is stored in kr (real part) and
-          ki (imaginary part)
-        */
-        PetscCall(EPSGetEigenvalue(eps, i, &kr, &ki));
-        /*
-          Compute the relative error associated to each eigenpair
-        */
-        PetscCall(EPSComputeError(eps,i,EPS_ERROR_RELATIVE,&error));
-
-        PetscCall(PetscPrintf(PETSC_COMM_WORLD,"   %12f       %12g\n",(double)kr,(double)error));
-      }
-      PetscCall(PetscPrintf(PETSC_COMM_WORLD,"\n"));
+    PetscCheck(nconv >= N_evs, PETSC_COMM_WORLD, 1, "did not converge");
+    PetscCall(PetscFPrintf(PETSC_COMM_WORLD, file, "%.5g\t", Phi));
+    for (i = 0; i < N_evs; ++i) {
+      PetscCall(EPSGetEigenvalue(eps, i, &kr, &ki));
+      PetscCall(PetscFPrintf(PETSC_COMM_WORLD, file, "%.5g\t", (double ) kr));
     }
+    PetscCall(PetscFPrintf(PETSC_COMM_WORLD, file, "\n"));
   }
-  /*
-     Free work space
-  */
-  /* PetscCall(EPSDestroy(&eps)); */
-  /* PetscCall(MatDestroy(&H)); */
-  /* PetscCall(VecDestroy(&xr)); */
-  /* PetscCall(VecDestroy(&xi)); */
-  /* PetscCall(SlepcFinalize()); */
+
   return 0;
 }
